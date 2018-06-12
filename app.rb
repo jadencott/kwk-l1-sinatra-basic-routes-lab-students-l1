cc880433e2aa853acc6906c54d109cc0681e49f9
Jaden <jcottorone21405@gmail.com>
2018-06-12 17:44:46 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+get '/name' do
+  "My name is Jaden"
+end
+
+get '/hometown' do
+  "My hometown is Centennial"
+end
+
+get '/favorite-song'do 
+  "My favorite song is Mr. Brightside"
+end
+
+
+
+
 end
